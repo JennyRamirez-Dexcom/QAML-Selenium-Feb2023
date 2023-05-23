bbf56e8d527cebb08ac9234a8c098d5f9baeb65b
Jennifer Ramirez <jxr20920@dexcom.com>
2023-05-22 20:39:25 -0700

    properties files - configuration files

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,16 +2,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class BaseTest {
     protected WebDriver webDriver;
 
     //Siempre private o protected NO PUBLIC
     protected WebDriver getWebDriver() {
-        File ruta = new File("/Users/jxr20920/Downloads/geckodriver-2");
+        String rutaFireFoxDriver = getProperty("FIREFOX_DRIVER_PATH");
+        File ruta = new File(rutaFireFoxDriver);
         System.setProperty("webdriver.gecko.driver", ruta.getPath());
         //System.setProperty("webdriver.chrome.driver", ruta.getPath());
         webDriver = new FirefoxDriver();
         return webDriver;
+    }
+
+    public String getProperty(String key) {
+        Properties properties = new Properties();
+        InputStream inputStream;
+        String propertyValue = null;
+        try{
+            inputStream = new FileInputStream("/Users/jxr20920/QAML-Feb2023/QAML-Selenium-May-6/settings.properties");
+            properties.load(inputStream);
+            propertyValue = properties.getProperty(key);
+            inputStream.close();
+        } catch (IOException ioException) {
+
+        } finally {
+
+        }
+        return propertyValue;
     }
 }
