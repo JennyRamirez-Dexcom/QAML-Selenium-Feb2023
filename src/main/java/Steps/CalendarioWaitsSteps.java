2456911c2314504ae0a795e86cfcc20fda57027b
Abraham Miranda <abraham.miranda.rodriguez@gmail.com>
2023-05-22 21:15:25 -0600

    Commit Clase 22 Mayo 2023

---
+++ src/main/java/Steps/CalendarioWaitsSteps.java
@@ -0,0 +1,44 @@
+package Steps;
+
+import Pages.CalendarioWaitsPage;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CalendarioWaitsSteps extends BaseSteps {
+    public CalendarioWaitsSteps(WebDriver driver) {
+        super(driver);
+    }
+    CalendarioWaitsPage calendarioWaitsPage = PageFactory.initElements(webDriver, CalendarioWaitsPage.class);
+
+    public void abrirPaginaCalendario() {
+        webDriver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
+    }
+    public void clickMay22() {
+        calendarioWaitsPage.getTdMay22().click();
+    }
+
+    public void waitFechaSeleccionada() {
+        Wait<WebDriver> fluentWait = new FluentWait<>(webDriver)
+                .withTimeout(Duration.ofSeconds(60))
+                .pollingEvery(Duration.ofSeconds(10))
+                .ignoring(NoSuchElementException.class);
+        fluentWait.until(webDriver1 -> {
+            return calendarioWaitsPage.getSpanMay22().isDisplayed();
+        });
+    }
+
+    public void waitFechaSeleccionadaExplicit() {
+        WebElement explicitWait = new WebDriverWait(webDriver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.visibilityOf(
+                        calendarioWaitsPage.getSpanMay22()
+                ));
+    }
+}
---
+++ src/test/java/WaitsTests.java
@@ -0,0 +1,21 @@
+import Steps.CalendarioWaitsSteps;
+import org.testng.annotations.Test;
+
+public class WaitsTests extends BaseTest {
+    CalendarioWaitsSteps calendarioWaitsSteps = new CalendarioWaitsSteps(webDriver);
+    @Test
+    public void calendarioWaitTest() {
+        calendarioWaitsSteps.abrirPaginaCalendario();
+        calendarioWaitsSteps.clickMay22();
+        calendarioWaitsSteps.waitFechaSeleccionada();
+        calendarioWaitsSteps.finalizarWebDriver();
+    }
+    @Test
+    public void calendarioExplicitWaitTest() {
+        calendarioWaitsSteps.abrirPaginaCalendario();
+        calendarioWaitsSteps.clickMay22();
+        calendarioWaitsSteps.waitFechaSeleccionadaExplicit();
+        calendarioWaitsSteps.finalizarWebDriver();
+    }
+
+}
