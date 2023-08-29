fd7fa4765cb79cdca8f2715e7b4c1a373cefe159
Diana F <Diafis02@gmail.com>
2023-08-29 12:29:58 -0500

    Index TC
    Addding TC for available cards

--- src/main/java/Steps/DemoBlazeSteps.java
+++ src/main/java/Steps/DemoBlazeSteps.java
@@ -1,9 +1,8 @@
 package Steps;
 
-import Pages.DemoBlazeHeaderPage;
-import Pages.DemoBlazeIndexPage;
-import Pages.DemoBlazeProdPage;
-import Pages.DemoBlazeShoppingCartPage;
+import Helpers.Card;
+import Pages.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,40 +13,122 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class DemoBlazeSteps extends BaseSteps{
+public class DemoBlazeSteps extends BaseSteps {
     DemoBlazeIndexPage demoBlazeIndexPage = PageFactory.initElements(webDriver, DemoBlazeIndexPage.class);
     DemoBlazeProdPage demoBlazeProdPage = PageFactory.initElements(webDriver, DemoBlazeProdPage.class);
-    DemoBlazeHeaderPage demoBlazeHeaderPage = PageFactory.initElements(webDriver, DemoBlazeHeaderPage.class);
+    DemoBlazeMainFixOptionsPage demoBlazeOptionsPage = PageFactory.initElements(webDriver, DemoBlazeMainFixOptionsPage.class);
     DemoBlazeShoppingCartPage demoBlazeShoppingCartPage = PageFactory.initElements(webDriver, DemoBlazeShoppingCartPage.class);
+    DemoBlazeLoginPage demoBlazeLoginPage = PageFactory.initElements(webDriver, DemoBlazeLoginPage.class);
 
     public DemoBlazeSteps(WebDriver driver) {
         super(driver);
     }
 
-    private void waitForElementToBeCompleted() {
+    private void waitForListOfItemsToLoad() {
         Wait<WebDriver> fluentWait = new FluentWait<>(webDriver)
-                .withTimeout(Duration.ofSeconds(10))
+                .withTimeout(Duration.ofSeconds(30))
                 .pollingEvery(Duration.ofSeconds(5))
                 .ignoring(NoSuchElementException.class);
 
         fluentWait.until(webDriver1 -> {
-            String cartsListsize = String.valueOf(demoBlazeIndexPage.getAvailableCardsInPage().size());
-            return cartsListsize.equals("9");
+            int cartsListsize = demoBlazeIndexPage.getAvailableCardsInPage().size();
+            return (cartsListsize > 0);
         });
     }
 
-    public List<WebElement> findCardsListInFirstPage(){
-        waitForElementToBeCompleted();
-         List<WebElement> cardsList = demoBlazeIndexPage.getAvailableCardsInPage();
-         return cardsList;
+
+    //Get Elements available in current page version 2 get all information fill
+    public List<Card> getAvailableItems(){
+        waitForListOfItemsToLoad();
+        List<Card> listOFItemsAvailable = new ArrayList<>();
+            listOFItemsAvailable = demoBlazeIndexPage.getAvailableCardsInPageInfo();
+            printOut("QA-- Elements available in current page " + listOFItemsAvailable.size());
+            for (Card card : listOFItemsAvailable) {
+                printOut("Test cards " + card.getCardName());
+                printOut(card.getDescription());
+                printOut(card.getPrice());
+                printOut(card.getImageRef());
+            }
+        return listOFItemsAvailable;
     }
+
+    //Get Elements available in current page version 1 whole element
+    private List<WebElement> findCardsListedInCurrentPage() {
+        waitForListOfItemsToLoad();
+        List<WebElement> cardsList = null;
+        cardsList = demoBlazeIndexPage.getAvailableCardsInPage();
+        printOut("ListSize in current page: " + cardsList.size());
+        return cardsList;
+    }
+
+    public List<Card> getAllAvailableItemsToPurchase() {
+        List<WebElement> listAllAvalItems = new ArrayList<>();
+        List<WebElement> listAvalItemsInPage = findCardsListedInCurrentPage();
+        listAllAvalItems.addAll(listAvalItemsInPage);
+        //NEW
+        List<Card> listOfCards = demoBlazeIndexPage.getAvailableCardsInPageInfo2(listAllAvalItems);
+
+        do{
+            getToNextPage();
+           waitForElementToNotBeVisible(listAvalItemsInPage.get(0));
+            listAvalItemsInPage = findCardsListedInCurrentPage();
+            List<Card> availableCardsInPageInfo2 = demoBlazeIndexPage.getAvailableCardsInPageInfo2(listAvalItemsInPage);
+            listOfCards.addAll(availableCardsInPageInfo2);
+            listAllAvalItems.addAll(listAvalItemsInPage);
+         }while (demoBlazeIndexPage.getNextPageButton().isDisplayed());
+        printOut("List of items quantity accumulated: "+listAllAvalItems.size());
+
+        //NEW CODE HERE
+        //List<Card> listOfCards = demoBlazeIndexPage.getAvailableCardsInPageInfo2(listAllAvalItems);
+        //return listOfCards;
+
+        //Change the return
+        //return listAllAvalItems;
+
+        return listOfCards;
+                //demoBlazeIndexPage.getAvailableCardsInPageInfo();
+    }
+
+
+
+    public void getToNextPage(){
+        scrollToElement(demoBlazeIndexPage.getNextPageButton());
+        demoBlazeIndexPage.getNextPageButton().click();
+    }
+
+    public void getPhoneItems(){
+        demoBlazeOptionsPage.getPhonesMenuOption().click();
+        List<WebElement> phonesAvailableList = findCardsListedInCurrentPage();
+        Map<String,String> availablePhonesWithPrices = new HashMap<String,String>();
+        for(WebElement phone:phonesAvailableList){
+            availablePhonesWithPrices.put(phone.getText(),
+                    phone.getAttribute(phone.findElement(By.xpath("//a[@class='hrefch']")).getText()));//findElement(By.xpath("//h4[@class='card-title']/h5"))
+                    printOut(availablePhonesWithPrices.toString());
+        }
+
+    }
+
+    public void getIphonesAvailableInformation(){
+        List<Card> cardsInfo= demoBlazeIndexPage.getAvailableCardsInPageInfo();
+        printOut("QA"+cardsInfo.size());
+        for(Card card : cardsInfo ){
+            printOut(card.getCardName());
+            printOut(card.getDescription());
+        }
+
+    }
+
+
 
     public void findCardByTextAndClick(String device){
         int cont=0;
-        List<WebElement> cardsList= findCardsListInFirstPage();
+        List<WebElement> cardsList= findCardsListedInCurrentPage();
         for(WebElement card: cardsList  ){
             boolean contains = card.getText().contains(device);
             if(contains){
@@ -80,7 +161,7 @@ public class DemoBlazeSteps extends BaseSteps{
     }
 
     private void waitForVisibleAfter5SecondsButtontoBeDisplayed(){
-        WebElement explicitWait = new WebDriverWait(webDriver, Duration.ofSeconds(5))
+        WebElement explicitWait = new WebDriverWait(webDriver, Duration.ofSeconds(30))
                 .until(ExpectedConditions.visibilityOf(demoBlazeProdPage.getAddToCartBtn()));
     }
 
@@ -89,7 +170,7 @@ public class DemoBlazeSteps extends BaseSteps{
     }
 
     public void clickAddToCart(){
-        WebElement explicitWait = new WebDriverWait(webDriver, Duration.ofSeconds(5))
+        WebElement explicitWait = new WebDriverWait(webDriver, Duration.ofSeconds(30))
                 .until(ExpectedConditions.visibilityOf(demoBlazeProdPage.getAddToCartBtn()));
         demoBlazeProdPage.getAddToCartBtn().click();
     }
@@ -103,16 +184,37 @@ public class DemoBlazeSteps extends BaseSteps{
      }
 
     public void goToCartMenuOption(){
-        WebElement explicitWait = new WebDriverWait(webDriver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOf(demoBlazeHeaderPage.getCartMenuOotion()));
-        demoBlazeHeaderPage.getCartMenuOotion().click();
+        WebElement explicitWait = new WebDriverWait(webDriver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.visibilityOf(demoBlazeOptionsPage.getCartMenuOption()));
+        demoBlazeOptionsPage.getCartMenuOption().click();
+    }
+
+    public void goToLoginMenuOption(String user, String password){
+        WebElement explicitWait = new WebDriverWait(webDriver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.visibilityOf(demoBlazeOptionsPage.getLoginMenuOption()));
+        demoBlazeOptionsPage.getLoginMenuOption().click();
+        WebElement explicitWait1 = new WebDriverWait(webDriver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.visibilityOf(demoBlazeLoginPage.getLoginUsername()));
+        demoBlazeLoginPage.getLoginUsername().sendKeys(user);
+        demoBlazeLoginPage.getPassword().sendKeys(password);
+        demoBlazeLoginPage.getLoginButton().click();
+    }
+
+    public String getWelcomeMessage(){
+        WebElement explicitWait2 = new WebDriverWait(webDriver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.visibilityOf(demoBlazeLoginPage.getWelcomeMessage()));
+        return  demoBlazeLoginPage.getWelcomeMessage().getText();
     }
 
     public List<WebElement> getListOfItemsInShoppingCart(){
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         List<WebElement> devicesToPurchaseList =  demoBlazeShoppingCartPage.getShoppingCartDevicesList();
         return devicesToPurchaseList;
     }
+
+   public List<String> getMenuOptions(){
+       return getOptionsTextFromAList(demoBlazeOptionsPage.getMenuOptionsHeader());
+   }
 
     public boolean validateISDeviceNameAndPriceInShoppingCart(String device, String price){
        boolean itemInList =false;
@@ -154,6 +256,10 @@ public class DemoBlazeSteps extends BaseSteps{
            sum = sum + Integer.parseInt(price.getText());
        }
        return sum;
+    }
+
+    public void waitForPreviousButtonToBeVisible(){
+        waitForElementToBeVisible(demoBlazeIndexPage.getPrevButton());
     }
 
 
--- src/test/java/DemoBlazeTest.java
+++ src/test/java/DemoBlazeTest.java
@@ -1,24 +1,66 @@
+import Helpers.Card;
 import Steps.DemoBlazeSteps;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DemoBlazeTest extends BaseTest{
 
     public WebDriver webDriver = getWebDriver("Chrome");
     DemoBlazeSteps demoBlazeSteps = new DemoBlazeSteps(webDriver);
+
     String Iphone632gb = "Iphone 6 32gb";
     String expectedPriceIphone632gb = "$790 *includes tax";
     String priceIphone632gb = "790";
     String expectedDescriptionIphone632gb = "Product description\n" +
             "It comes with 1GB of RAM. The phone packs 16GB of internal storage cannot be expanded. As far as the cameras are concerned, the Apple iPhone 6 packs a 8-megapixel primary camera on the rear and a 1.2-megapixel front shooter for selfies.";
     String expectedImageLinkIphone632gb = "https://www.demoblaze.com/imgs/iphone_6.jpg";
-
+    //List<String> list= Arrays.asList("Home (current)", "Contact", "About us", "Cart", "Log in", "Sign up");
+    List<String> publicMenuOptionsExpected = new ArrayList<String>() {{
+        add("Home (current)");
+        add("Contact");
+        add("About us");
+        add("Cart");
+        add("Log in");
+        add("Sign up");
+    } };
     @BeforeTest (alwaysRun = true)
     public void inizialization(){
         demoBlazeSteps.getToDemoBlazePage();
+    }
+
+    @Test(priority = 1, groups = {"Sanity"})
+    public void validateMenuOptionsGuessMode (){
+        List<String> menuOptions = demoBlazeSteps.getMenuOptions();
+        demoBlazeSteps.printOut("Options found" + menuOptions);
+        Assert.assertEquals(menuOptions,publicMenuOptionsExpected);
+    }
+
+    @Test (priority = 2, groups = "Saniyt")
+    public void loginSucess(){
+        demoBlazeSteps.goToLoginMenuOption("QADEMOBLAZE", "123");
+        Assert.assertEquals(demoBlazeSteps.getWelcomeMessage(), "Welcome QADEMOBLAZE");
+    }
+
+    @Test(description = "get ALL available items to purchase", priority = 1,groups = {"EndToEndTest"})
+    public void validateAvailableItems(){
+        //Assert.assertEquals(demoBlazeSteps.getAllAvailableItemsToPurchase().size(),15);
+        //List<Card> availableItems = demoBlazeSteps.getAvailableItems();
+        List<Card> allItemsList = demoBlazeSteps.getAllAvailableItemsToPurchase();
+        demoBlazeSteps.printOut("TEST:  " + allItemsList.size());
+        for (Card card : allItemsList){
+            demoBlazeSteps.printOut(card.getImageRef());
+            demoBlazeSteps.printOut(card.getCardName());
+            demoBlazeSteps.printOut(card.getDescription());
+            demoBlazeSteps.printOut(card.getPrice());
+        }
     }
 
     @Test (description = "validate product details for Iphone 6 32gb" , priority = 1,groups = {"EndToEndTest"})
@@ -71,7 +113,7 @@ public class DemoBlazeTest extends BaseTest{
 
     @AfterTest (alwaysRun = true)
     public void closeBrowser(){
-        demoBlazeSteps.cerrarVentana();
+       demoBlazeSteps.cerrarVentana();
     }
 
 
